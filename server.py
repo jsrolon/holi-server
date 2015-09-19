3d89b65e933cdededd2b84df2b863f0d19d21e3a
Juan Sebastián Rolón <js.rolon554@uniandes.edu.co>
2015-09-19 13:26:21 -0500

    dual server

--- server.py
+++ server.py
@@ -1,9 +1,38 @@
+# Server implementation
+
 from socket import *
+import threading
+
 serverPort = 1337
-serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-print("The server is ready to receive")
-while 1:
-    message, clientAddress = serverSocket.recvfrom(2048) 
-    modifiedMessage = message.upper() 
-    serverSocket.sendto(modifiedMessage, clientAddress)
+
+udpSocket = socket(AF_INET, SOCK_DGRAM)
+udpSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+tcpSocket = socket(AF_INET, SOCK_STREAM)
+tcpSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+udpSocket.bind(('', serverPort))
+print("UDP socket bound")
+tcpSocket.bind(('', serverPort))
+print("TCP socket bound")
+
+def listenUDP():
+    print("UDP socket listening")
+    while 1:
+        message, clientAddress = udpSocket.recvfrom(2048) 
+        modifiedMessage = message.upper() 
+        udpSocket.sendto(modifiedMessage, clientAddress)
+
+def listenTCP():
+    tcpSocket.listen(0)
+    print("TCP socket listening")
+    while 1:
+        connectionSocket, addr = tcpSocket.accept()
+        message = connectionSocket.recv(2048)
+        capitalizedSentence = message.upper()
+        connectionSocket.send(capitalizedSentence)
+        connectionSocket.close()
+
+udpThread = threading.Thread(target=listenUDP, args=())
+tcpThread = threading.Thread(target=listenTCP, args=())
+udpThread.start()
+tcpThread.start()
